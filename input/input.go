a9c899215f35a59c3021f8a8b4d88b4b3412d0b3
Matthew Gleich <matthewgleich@gmail.com>
2020-05-22 12:14:15 -0400

    🚸 Add user check
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

---
+++ input/input.go
@@ -0,0 +1,19 @@
+package input
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"syscall"
+)
+
+// CheckWithUser ... Make sure the user want to quit all application
+func CheckWithUser() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Are you sure you want to force quite all\napplications? (y or n)")
+	continueProgram, _ := reader.ReadString('\n')
+	if strings.ToLower(continueProgram) == "n\n" {
+		syscall.Exit(0)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/Matt-Gleich/nuke/input"
+)
+
+func main() {
+	input.CheckWithUser()
+}
