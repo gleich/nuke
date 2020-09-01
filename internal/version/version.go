8bf438ab77b083e12cf4ade44a66172dc8c96342
Matthew Gleich <matthewgleich@gmail.com>
2020-09-01 12:20:19 -0400

    🔖 Prep for v5.0.0 release
    Signed-off-by: Matthew Gleich <matthewgleich@gmail.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -16,7 +16,7 @@ func CheckForUpdate() {
 	s.Start()
 
 	const repoURL = "https://github.com/Matt-Gleich/nuke"
-	isOutdated, version, err := new_release.Check("v4.4.1", repoURL)
+	isOutdated, version, err := new_release.Check("v5.0.0", repoURL)
 	if err != nil {
 		statuser.Error("Failed to get current version number", err, 1)
 	}
